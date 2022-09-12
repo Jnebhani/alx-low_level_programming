142ee3dc585234964c8c3eff3ed1535d5d01199f
Jibril Nebhani <abbakotif@gmail.com>
2022-09-12 11:44:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+*main - print the string in the put function 
+*
+*Description: using the main function 
+*this program prints "programming is like building multilingual puzzle
+*Return: 0
+/*
+{
+    printf("with proper grammer, but the outcome is a piece pf of art,\n");
+         return (0);
+}
