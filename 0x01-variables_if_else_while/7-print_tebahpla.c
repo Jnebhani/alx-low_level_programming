41fa590cde0507514033dc4f0d006f29c25bc196
Jibril Nebhani <abbakotif@gmail.com>
2022-09-12 18:05:59 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,13 +5,15 @@
 *
 *Return: Always (Success)
 */
+int main(void)
 {
-       char c;
-   for (c = 'z'; c >= 'a'; c--)
-{
-       putchar(c);
-}
-     putchar('\n');
- 
-   return (0);
+        char c;
+
+       for (c = 'z'; c >= 'a'; c--)
+      {
+
+          putchar(c);
+    }
+         putchar('\n');
+  return (0);
 }
