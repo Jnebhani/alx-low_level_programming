46785c8de7e52eb897aec0b4e9d371c2ffaa9e6f
Jibril Nebhani <abbakotif@gmail.com>
2022-09-14 06:13:35 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,51 +1,21 @@
-nclude "main.h"
+#include "main.h"
 
-#ifndef HOLBERTON_H_INCLUDED
-
-#define HOLBERTON_H_INCLUDED
-
-int _purchar(char);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int_islower(int c);
-
-int_isalpha(int c);
-
-int print_sign(int n);
-
-int_abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-void num(int r, int n);
-
-		       
-       #endif
-
-#include <main.h>
 /**
- *
- * _putchar - writes the character c to stduot
- * @c: The character to print
- *
- * Return: on success 1.
- * on error, -1 is returned, and errno is set appropraitely.
+ * main - print putchar
+ * Return:always 0
  */
-int _putchar(char c)
+int main(void)
 {
- return(write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
 
