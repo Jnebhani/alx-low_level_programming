42388ce60df245cc3e36a612b802a6fa46c80309
Jnebhani <abbakotif@gmail.com>
2022-12-08 17:13:05 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
-
 /*
-* main - print the stringg[Dddddd[D[D[D[D[D[D[D[D[D
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,");
+	  return (0);
+}
