777549cdbfb30f52c66695b590b30be5d7f7c960
Jnebhani <abbakotif@gmail.com>
2022-12-15 15:50:51 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for a digit (0 through 9).
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	intt c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar('\n');
+	}
+}
