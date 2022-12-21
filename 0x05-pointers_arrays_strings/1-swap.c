4509cf77ace7873ccf02346c21ae821713baae3c
Jnebhani <abbakotif@gmail.com>
2022-12-20 18:01:36 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,10 +6,10 @@
  * @b: is the second integer
  * Return:always 0.
  */
-
 void swap_int(int *a, int *b);
 {
-	*a += *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	int temp = *a;
+	
+	*a = *b;
+	*b = temp;
 }
