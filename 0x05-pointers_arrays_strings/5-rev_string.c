88903aeefd92eafbe444a1e03392e91aa5249337
Jnebhani <abbakotif@gmail.com>
2022-12-30 09:06:00 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,17 @@
  */
 void rev_string(char *s);
 {
-	int i, j, k, temp:
-
-		i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	k = 0;
-	j = i - 1;
-	while (k < j)
-	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		k++;
-		j--;
+	char rev = s[0];
+	int counter = 0;
+	int i;
+	
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+       {
+	       counter--;
+		rev = s[i];
+		s[j] = s[counter];
+		s[counter] = rev;
 	}
 }
