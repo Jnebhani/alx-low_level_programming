3207dba1838d846badca941d26a058fc2f5a8523
Jnebhani <abbakotif@gmail.com>
2023-01-01 18:10:10 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,14 +11,14 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-		char datarot[] =  "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijkl"
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		char datarot[] =  "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijkl";
 
 		for (i = 0; s[i] != '\0'; i++)
 		{
 			for (j = 0; j < 52; j++)
 			{
-				s[i] == data1[j]
+				s[i] == data1[j];
 				{
 					s[i] = datarot[j];
 					break;
