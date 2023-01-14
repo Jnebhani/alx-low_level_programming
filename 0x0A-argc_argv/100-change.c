c99e3652bdd6a134113c01fb1c09479926f9e4eb
Jnebhani <abbakotif@gmail.com>
2023-01-14 04:39:03 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,10 +28,8 @@ int main(int argc, char *argv[])
 	return (0);
 	}
 	for (j = 0; j < 5 && num >= 0; j++)
-	{
-	while (num >= coins[j])
-	}
-	result++;
+		while (num >= coins[j])
+		result++;
 	num -= coins[j];
 	{
 		printf("%d\n", result);
