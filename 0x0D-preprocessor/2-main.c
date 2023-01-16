76946523035d3bf3d8f2fd1be98a34cf54b6da0f
Jnebhani <abbakotif@gmail.com>
2023-01-16 05:09:34 -0800

    #endif

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
- * that followed by new line
+ * main - entry point
+ *
+ * Description: prints the name of the file compiled from
+ *
  * Return: Always 0 (Success)
  */
 int ain(void)
