42af49c19bc2ace9da77cfa4bd61b8ef6afdf5fd
Jnebhani <abbakotif@gmail.com>
2023-01-16 18:58:31 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * ain -prints the name of the file followed by new line
+ * main -prints the name of the file followed by new line
  *
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
