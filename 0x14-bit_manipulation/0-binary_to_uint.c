b33cebea011c3c00f206fb2d76ed9ce0c63c1532
Jibril Nebhani <abbakotif@gmail.com>
2023-02-03 15:16:15 -0800

    0. 0 tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,10 +2,10 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: unsigned int.
- */
+  * @b: string containing the binary number
+  *
+  * Return: the converted number
+  */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
